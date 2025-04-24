2b6aadd057fdd43b63311b7be1b9f4f93766fa31
angelica-zach <angelscakeunicorn@gmail.com>
2025-04-24 12:16:02 +0100

    font and experience page

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -1,19 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/common/navBar";
 import Footer from "../components/common/footer";
 import "./styles/skills.css";
 
 const Skills = () => {
+	const items = [
+		{
+			title: "Languages",
+			content: ["CSS", "HTML", "JavaScript", "SASS", "C"],
+		},
+		{
+			title: "Frameworks + Technologies",
+			content: ["Node.js", "React", "JQuery", "Matlab"],
+		},
+		{
+			title: "Tools + Platforms",
+			content: [
+				"Bootstrap",
+				"Figma",
+				"Notion",
+				"Jira",
+				"Netlify",
+				"GitHub",
+				"Hubspot",
+				"Eventmaker",
+			],
+		},
+		{
+			title: "Other",
+			content: ["Fluent in Polish", "Microsoft office"],
+		},
+	];
+
+	const [openIndex, setOpenIndex] = useState(null);
+	const [hoveredIndex, setHoveredIndex] = useState(null);
+	const toggleContent = (index) => {
+		setOpenIndex(openIndex === index ? null : index);
+	};
+
 	return (
 		<React.Fragment>
 			<div className="page-content">
 				<NavBar active="skills" />
 				<div className="content-wrapper">
-					<div className="contact-container">
-						<div className="title contact-title">My Skills</div>
+					<div className="contact-container skill-container">
+						<div className="title skills-title">My Skills</div>
 						<div className="section-content-container">
 							<br />
-							<div className="container-skills">
+							{/* <div className="container-skills">
 								<h3 className="title-skills">Languages</h3>
 								<div className="skill-box">
 									<p>CSS</p>
@@ -52,6 +86,43 @@ const Skills = () => {
 								<div className="skill-box">
 									<p>Fluent in Polish</p>
 									<p>Microsoft Office</p>
+								</div>
+							</div> */}
+							<div
+								key={index}
+								onClick={() => toggleContent(index)}
+								onMouseEnter={() => setHoveredIndex(index)}
+								onMouseLeave={() => setHoveredIndex(null)}
+								style={{
+									cursor: "pointer",
+									marginBottom: "10px",
+									border: "1px solid #ccc",
+									padding: "10px",
+									borderRadius: "4px",
+									transition: "all 0.3s ease",
+									display: "flex",
+									alignItems: "center",
+								}}
+							>
+								<span
+									style={{
+										width: "20px",
+										marginRight: "10px",
+									}}
+								>
+									{hoveredIndex === index ? "➤" : ""}
+								</span>
+								<div className="container-skills">
+									<h3 className="title-skills">
+										{item.title}
+									</h3>
+									{openIndex === index && (
+										<div className="skill-box">
+											{item.content.map((para, i) => (
+												<p key={i}>{para}</p>
+											))}
+										</div>
+									)}
 								</div>
 							</div>
 						</div>
